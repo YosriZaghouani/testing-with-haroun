85a2d03a738f37bd92dfeb581d3c0f187d63f96d
Yosrii <50800084+YosriZaghouani@Yosri.noreply.github.com>
2021-03-01 17:13:30 +0100

    testing git with haroun

---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Users from './Singers'
+
+class Display extends Component {
+    render()
+    {
+        return(
+            <div>
+                <h1>Musiciens</h1>
+               <Users name ="Eric Clapton" age="74"/>
+               <Users name ="Jimi Shoe" age="27" />
+               <Users name ="David Gilmour" age="73"/>
+               <Users name ="Carlos Santana" age="71" />
+            </div>
+        )
+    }
+}
+
+export default Display;
---
+++ src/components/Singers.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Users = ({name , age}) => {
+
+    
+    console.log(name , age)
+
+    return (
+        <div>
+
+            <p>Chanteur:</p>
+           
+        </div>
+    )
+}
+
+export default Users;
